6cc27461a37806e6f4dbeb84cda7985deac49771
Vitor França <vitor.sousa.franca@gmail.com>
2025-06-09 19:30:35 -0300

    add: deal with copy errors adding a log file + skip stack if NiFTI file already exists

--- nnunetv2/equalized_dataset_test/copy_equalized_dataset.py
+++ nnunetv2/equalized_dataset_test/copy_equalized_dataset.py
@@ -4,6 +4,7 @@ import numpy as np
 import subprocess
 import SimpleITK as sitk
 import pydicom
+import logging
 
 class CopyEqualizedDataset:
     def __init__(self):
@@ -12,6 +13,18 @@ class CopyEqualizedDataset:
 
         self.destination_path = os.path.join('..','media','input_equalized_dataset_test')
         os.makedirs(self.destination_path, exist_ok=True)
+
+        # logger configuration
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        log_filename = os.path.join(current_dir, 'copy_errors.log')
+        logging.basicConfig(
+            filename=log_filename,
+            level=logging.ERROR,
+            format='%(asctime)s - %(message)s'
+        )
+        self.logger = logging.getLogger(__name__)
+
+
         
     def run_copy(self):
         # Iterate through each row in the DataFrame
@@ -26,22 +39,29 @@ class CopyEqualizedDataset:
             dummy_id = row['Dummy_ID']
             subfolder_L = row['Subfolder_L']
             subfolder_R = row['Subfolder_R']
+            birads_density = row['birads_density']
 
             origin_path_left = f'/mnt/rede/{dummy_id}/PROC_Tomo_RC/{subfolder_L}'
             origin_path_right= f'/mnt/rede/{dummy_id}/PROC_Tomo_RC/{subfolder_R}'
-
-            # Command to copy the folder
             print(f"{index}/{nb_rows}", flush=True)
-            command = f"cp -r {origin_path_left} {self.destination_path}"
-            # Execute the command
-            subprocess.run(command, shell=True, check=True)    
-            print(f"Copied {subfolder_L} to {self.destination_path}", flush=True)
-
-            command = f"cp -r {origin_path_right} {self.destination_path}"
-            # Execute the command
-            subprocess.run(command, shell=True, check=True)
-            print(f"Copied {subfolder_R} to {self.destination_path}", flush=True)
-
+            # Command to copy the folder
+            try:
+                command = f"cp -r {origin_path_left} {self.destination_path}"
+                # Execute the command
+                subprocess.run(command, shell=True, check=True)    
+            except subprocess.CalledProcessError as e:
+                print(f"[ERROR] Failed to copy {subfolder_L}", flush=True)
+                self.logger.error(f"Failed to copy {subfolder_L} from {origin_path_left} - BIRADS: {birads_density}")
+                continue
+            try:
+                command = f"cp -r {origin_path_right} {self.destination_path}"
+                # Execute the command
+                subprocess.run(command, shell=True, check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"[ERROR] Failed to copy {subfolder_R}", flush=True)
+                self.logger.error(f"Failed to copy {subfolder_R} from {origin_path_right} - BIRADS: {birads_density}")
+                continue
+            
 
     def stack_dicom_series(self):
         slices = []
@@ -52,6 +72,10 @@ class CopyEqualizedDataset:
                 continue
             
             output_file = os.path.join(self.destination_path, f"{folder}.nii.gz")
+            if os.path.exists(output_file):
+                print(f"[SKIP] NiFTI file already exists: {output_file}")
+                continue
+
             print(f"Processing folder: {input_folder}")
 
             if not os.path.exists(input_folder):
