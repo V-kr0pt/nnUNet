0c257d7ff63ac98d8d3bf086b5adf3ec2a033d3d
Vitor França <vitor.sousa.franca@gmail.com>
2025-08-20 13:58:50 -0300

    fix: add PlansFrom995 as argument for nnUNetv2_train

--- nnunetv2/run/finetunning.sh
+++ nnunetv2/run/finetunning.sh
@@ -32,7 +32,7 @@ for FOLD in 0 1 2 3 4; do
     fi
 
     set -x
-    nnUNetv2_train "$FINETUNE_DS" "$CONFIG" "$FOLD" \
+    nnUNetv2_train "$FINETUNE_DS" "$CONFIG" "$FOLD" -p PlansFrom995\
         -pretrained_weights "$CKPT" 2>&1 | tee "$LOG_DIR/fold_${FOLD}.log"
     set +x
 
