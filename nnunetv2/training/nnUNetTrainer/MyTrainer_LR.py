d8a4b1a36f0c6432ec951aec031874577de78b95
Vitor França <vitor.sousa.franca@gmail.com>
2025-09-02 15:35:06 -0300

    fix: nnUNetTrainer args

--- nnunetv2/training/nnUNetTrainer/MyTrainer_LR.py
+++ nnunetv2/training/nnUNetTrainer/MyTrainer_LR.py
@@ -1,8 +1,9 @@
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
-
+import torch
 
 class MyTrainer_LR(nnUNetTrainer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, device)
         # Defining a lower initial learning rate
-        self.initial_lr = 3e-4   
+        self.initial_lr = 3e-4
