2e5428800937287f0d9028ea71ea604fa849c287
Vitor França <vitor.sousa.franca@gmail.com>
2025-09-05 08:59:04 -0300

    fix: MyTrainer_LR1e4_nowarmup inherets from nnUNetTrainer

--- nnunetv2/training/nnUNetTrainer/MyTrainer_LR1e4_nowarmup.py
+++ nnunetv2/training/nnUNetTrainer/MyTrainer_LR1e4_nowarmup.py
@@ -1,14 +1,10 @@
 import torch
-from nnunetv2.training.nnUNetTrainer.MyTrainer_LRWarmup10 import MyTrainer_LRWarmup10
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 
-class MyTrainer_LR1e4_nowarmup(MyTrainer_LRWarmup10):
+class MyTrainer_LR1e4_nowarmup(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
                  device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, device)
         
         # Default settings
         self.initial_lr = 1e-4
-        self.warmup_epochs = 0
-        self.use_warmup = False
-        self.freeze_encoder = False
-        self.freeze_epochs = 0
