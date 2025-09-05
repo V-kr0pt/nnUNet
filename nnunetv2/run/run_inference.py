b1b7e88f3b4ebdf9620b60bdc99d5bb088d994ef
Vitor França <vitor.sousa.franca@gmail.com>
2025-09-04 23:48:20 -0300

    add: force argument

--- nnunetv2/run/run_inference.py
+++ nnunetv2/run/run_inference.py
@@ -222,18 +222,19 @@ if __name__ == "__main__":
     parser.add_argument('--skip_pre', action='store_true', help='Skip preprocessing before inference')
     parser.add_argument('--skip_post', action='store_true', help='Skip postprocessing after inference')
     parser.add_argument('--restart_preprocess', action='store_true', help='Restart preprocessing even if it was done before')    
+    parser.add_argument('--force', action='store_true', help='Force running inference without confirmation')
     args = parser.parse_args()
     
 
     inference = Inference(args.input_folder, args.output_folder, args.dataset_id,
                           args.trainer, args.fold, args.skip_pre, args.skip_post,
                             args.restart_preprocess)
-    
-    confirm_inf_model_str = f'Are sure you want to run inferece for {inference.input_folder} -> {inference.output_folder}\n'
-    confirm_inf_model_str += f'using the model with ID {inference.dataset_ID} ? (Y/n): '
-    
-    if input(confirm_inf_model_str) == 'n':
-        print('Change configs and rerun.')
-        exit()
+    if args.force:
+        confirm_inf_model_str = f'Are sure you want to run inferece for {inference.input_folder} -> {inference.output_folder}\n'
+        confirm_inf_model_str += f'using the model with ID {inference.dataset_ID} ? (Y/n): '
+        
+        if input(confirm_inf_model_str) == 'n':
+            print('Change configs and rerun.')
+            exit()
 
     inference.run()
